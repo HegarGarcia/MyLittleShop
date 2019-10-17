79f842d4f612a4a7712c61465efaa7d0e1965ed5
Hegar Garcia <hegargarcia@gmail.com>
2019-10-17 09:00:59 -0500

    Renaming Activity Class name

--- app/src/main/java/com/hegargarcia/mylittleshop/LogInActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/LogInActivity.kt
@@ -3,7 +3,7 @@ package com.hegargarcia.mylittleshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
