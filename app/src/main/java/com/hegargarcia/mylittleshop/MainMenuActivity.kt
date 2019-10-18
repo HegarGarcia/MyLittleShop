9ba9ebdc34416b7dc82434e259202214be31d272
Ari Sepulveda <asepulveda1@ucol.mx>
2019-10-18 11:52:00 -0500

    Adding Main List Activity

---
+++ app/src/main/java/com/hegargarcia/mylittleshop/MainMenuActivity.kt
@@ -0,0 +1,29 @@
+package com.hegargarcia.mylittleshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main_menu.*
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+
+        botonCliente.setOnClickListener{
+            val intento = Intent(this,ClientListActivity::class.java)
+            startActivity(intento)
+        }
+
+        botonInventario.setOnClickListener{
+            val intento = Intent(this,ProductListActivity::class.java)
+            startActivity(intento)
+        }
+
+        botonVentas.setOnClickListener{
+            val intento = Intent(this,SellListActivity::class.java)
+            startActivity(intento)
+        }
+    }
+}
