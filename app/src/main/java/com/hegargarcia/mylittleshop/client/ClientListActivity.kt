d58fe1de8a499d37c82373ff1c00875d337c1252
Ari Sepulveda <asepulveda1@ucol.mx>
2019-10-19 10:36:09 -0500

    adding client list UI

--- app/src/main/java/com/hegargarcia/mylittleshop/client/ClientListActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/client/ClientListActivity.kt
@@ -3,11 +3,18 @@ package com.hegargarcia.mylittleshop.client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hegargarcia.mylittleshop.R
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_client_list.*
 
 class ClientListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_list)
+
+        addClientButton.setOnClickListener{
+            val intento = Intent(this,ClientFormActivity::class.java)
+            startActivity(intento)
+        }
     }
 }
