03350287aab992a361c185423a56ae7ba5907a24
Ari Sepulveda <asepulveda1@ucol.mx>
2019-10-19 12:09:24 -0500

    Add product list UI and addProduct() function

--- app/src/main/java/com/hegargarcia/mylittleshop/product/ProductFormActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/product/ProductFormActivity.kt
@@ -3,11 +3,41 @@ package com.hegargarcia.mylittleshop.product
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hegargarcia.mylittleshop.R
+import com.hegargarcia.mylittleshop.dao.ProductDao
+import com.hegargarcia.mylittleshop.database.AppDatabase
+import com.hegargarcia.mylittleshop.entity.Product
+import kotlinx.android.synthetic.main.activity_product_form.*
 
 class ProductFormActivity : AppCompatActivity() {
+
+    private var db: AppDatabase? = null
+    private var productDao: ProductDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_form)
+
+        db = AppDatabase?.getDatabase(this)
+        productDao = db?.product()
+
+        saveButton.setOnClickListener{
+            addProduct()
+        }
+
+        cancelButton.setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun addProduct(){
+        val product = Product(
+            name = namePrompt.text.toString(),
+            description = descriptionPrompt.text.toString(),
+            amount = amountPrompt.text.toString().toInt(),
+            price = pricePrompt.text.toString().toFloat(),
+            cost = costPrompt.text.toString().toFloat(),
+            photoUrl = photoPrompt.text.toString()
+        )
+        productDao?.insert(product)
     }
 }
--- app/src/main/java/com/hegargarcia/mylittleshop/product/ProductListActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/product/ProductListActivity.kt
@@ -1,13 +1,20 @@
 package com.hegargarcia.mylittleshop.product
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hegargarcia.mylittleshop.R
+import kotlinx.android.synthetic.main.activity_product_list.*
 
 class ProductListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_list)
+
+        addProductButton.setOnClickListener{
+            val intento = Intent(this, ProductFormActivity::class.java)
+            startActivity(intento)
+        }
     }
 }
