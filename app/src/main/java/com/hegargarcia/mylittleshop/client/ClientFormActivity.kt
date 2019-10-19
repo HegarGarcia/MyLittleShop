a26967a65b87605e7aa34617c289112a2ad01c3d
Ari Sepulveda <asepulveda1@ucol.mx>
2019-10-19 10:37:13 -0500

    adding client form UI and addClient() function

--- app/src/main/java/com/hegargarcia/mylittleshop/client/ClientFormActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/client/ClientFormActivity.kt
@@ -3,11 +3,40 @@ package com.hegargarcia.mylittleshop.client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hegargarcia.mylittleshop.R
+import com.hegargarcia.mylittleshop.dao.ClientDao
+import com.hegargarcia.mylittleshop.database.AppDatabase
+import com.hegargarcia.mylittleshop.entity.Client
+import kotlinx.android.synthetic.main.activity_client_form.*
 
 class ClientFormActivity : AppCompatActivity() {
+
+    private var db: AppDatabase? = null
+    private var clientDao: ClientDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_form)
+
+        db = AppDatabase?.getDatabase(this)
+        clientDao = db?.client()
+
+        saveClientButton.setOnClickListener {
+            addClient()
+        }
+
+        cancelButton.setOnClickListener{
+            finish()
+        }
+    }
+
+    private fun addClient() {
+
+        val client = Client(
+            name = namePrompt.text.toString(),
+            address = addressPrompt.text.toString(),
+            email = emailPrompt.text.toString(),
+            phone = phonePrompt.text.toString()
+        )
+        clientDao?.insert(client)
     }
 }
