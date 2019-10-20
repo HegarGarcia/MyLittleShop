565f2be441e5db9f4af1e88f2756a4c677bab2fd
Hegar Garcia <hegargarcia@gmail.com>
2019-10-19 23:56:21 -0500

    Added search and query between dates for Sells DAO

--- app/src/main/java/com/hegargarcia/mylittleshop/dao/SellDao.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/dao/SellDao.kt
@@ -2,6 +2,7 @@ package com.hegargarcia.mylittleshop.dao
 
 import androidx.room.*
 import com.hegargarcia.mylittleshop.entity.Sell
+import java.util.*
 
 @Dao
 interface SellDao {
@@ -16,4 +17,22 @@ interface SellDao {
 
     @Delete
     fun delete(sell: Sell)
+
+    @Query("SELECT * FROM Sell")
+    fun getAll(): List<Sell>
+
+    @Query("SELECT * FROM Sell WHERE date BETWEEN :from and :to")
+    fun findSellsBetweenDates(from: Date, to: Date): List<Sell>
+
+    @Query(
+        """
+        SELECT *, Product.description as productDescription, Client.name as clientName FROM Sell
+        INNER JOIN Product
+        ON Sell.product = Product.id
+        INNER JOIN Client
+        ON Sell.client = Client.id
+        WHERE productDescription LIKE :searchString OR clientName LIKE :searchString
+        """
+    )
+    fun search(searchString: String): List<Sell>
 }
