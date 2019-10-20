3210d297fc75b4fe4e66efaef35343249795e317
Hegar Garcia <hegargarcia@gmail.com>
2019-10-20 00:25:17 -0500

    Fixing conflicts

--- app/src/main/java/com/hegargarcia/mylittleshop/client/ClientListActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/client/ClientListActivity.kt
@@ -3,7 +3,6 @@ package com.hegargarcia.mylittleshop.client
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hegargarcia.mylittleshop.R
-import android.content.Intent
 import kotlinx.android.synthetic.main.activity_client_list.*
 
 class ClientListActivity : AppCompatActivity() {
@@ -11,10 +10,14 @@ class ClientListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_client_list)
+    }
 
-        addClientButton.setOnClickListener{
-            val intento = Intent(this,ClientFormActivity::class.java)
-            startActivity(intento)
+    private fun setUpToolbar() {
+        toolbar.title = getString(R.string.client_list_title)
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp)
+
+        toolbar.setNavigationOnClickListener {
+            finish()
         }
     }
 }
