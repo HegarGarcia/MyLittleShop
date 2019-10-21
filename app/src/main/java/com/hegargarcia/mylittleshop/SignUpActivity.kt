b2302e6f4c76c87a485b6dfc544bb964a1d269d9
Hegar Garcia <hegargarcia@gmail.com>
2019-10-21 18:00:23 -0500

    One store per device

--- app/src/main/java/com/hegargarcia/mylittleshop/SignUpActivity.kt
+++ app/src/main/java/com/hegargarcia/mylittleshop/SignUpActivity.kt
@@ -1,32 +1,41 @@
 package com.hegargarcia.mylittleshop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.hegargarcia.mylittleshop.authentication.Auth
+import com.hegargarcia.mylittleshop.dao.UserDao
+import com.hegargarcia.mylittleshop.database.AppDatabase
 import com.hegargarcia.mylittleshop.entity.User
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
+    private var userDao: UserDao? = null
     private var auth: Auth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        userDao = AppDatabase.getDatabase(this)?.user()
         auth = Auth(this)
 
-        signUpButton.setOnClickListener{
+        userDao?.getById(1)!!.run {
+            storeNamePrompt.setText(storeName)
+            storeNamePrompt.isEnabled = false
+        }
+
+        signUpButton.setOnClickListener {
             addUser()
         }
 
-        cancelButton.setOnClickListener{
+        cancelButton.setOnClickListener {
             finish()
         }
     }
 
-    private fun addUser(){
+    private fun addUser() {
         val user = User(
             username = userPrompt.text.toString(),
             password = passwordPrompt.text.toString(),
@@ -34,7 +43,7 @@ class SignUpActivity : AppCompatActivity() {
             storeName = storeNamePrompt.text.toString()
         )
 
-        Toast.makeText(this,"Te has registrado con exito",Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Te has registrado con exito", Toast.LENGTH_LONG).show()
 
         auth?.signUp(user)
         finish()
